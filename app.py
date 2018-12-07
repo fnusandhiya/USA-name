5fd61be7c0d8c69668337a46c1158abe6fa1f7cc
sandhiya <sandhyasoneta12@gmail.com>
2018-12-06 21:45:25 -0500

    final

--- app.py
+++ app.py
@@ -7,7 +7,27 @@ from flask import (
     jsonify)
 
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///names_data.sqlite"
+
+db = SQLAlchemy(app)
+# Create our database model
+class SQL(db.Model):
+    __tablename__ = 'names'
+
+    index = db.Column(db.Integer, primary_key=True)
+    Abbr = db.Column(db.String)
+    Gender = db.Column(db.String)
+    Year = db.Column(db.Integer)
+    Name = db.Column(db.String)
+    Occurrence = db.Column(db.Integer)
+    State = db.Column(db.String)
+    Lat = db.Column(db.Float)
+    Lon = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<SQL %r>' % (self.Name)
 
 
 @app.route("/data")
@@ -23,6 +43,24 @@ def data():
 def home():
     """Render Home Page."""
     return render_template("index.html")
+
+@app.route("/sqlite")
+def sql():
+    """Render Home Page."""
+    query_statement = db.session.query(SQL.Name, SQL.Gender, SQL.Occurrence).statement
+    results = pd.read_sql_query(query_statement, db.session.bind)
+    json_results = results.to_json(orient='records')
+    return jsonify(json_results)
+
+@app.route("/search")
+def Search():
+    name = request.args.get('name')
+    query_statement = db.session.query(SQL.Name, SQL.Gender, SQL.Occurrence).filter(SQL.Name == name).statement
+    results = pd.read_sql_query(query_statement, db.session.bind)
+    json_results = results.to_json(orient='records')
+    return jsonify(json_results)
+
+
 
 
 
--- static/app.js
+++ static/app.js
@@ -1,5 +1,71 @@
-url = "/data"
+/*url = "/sqlite"
+var newData;
 d3.json(url).then(function(data) {
+    newData = JSON.parse(data);
+    //console.log(newData);
+});*/
+
+// Select the submit button
+var submit = d3.select("#submit");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#patient-form-input").property("value")
+  //console.log(inputElement);
+  d3.json('/search?name=' + inputElement).then(function(data) {
     var newData = JSON.parse(data);
     //console.log(newData);
+
+      // Get the value property of the input element
+  //var inputValue = inputElement.property("value");
+
+  //console.log(people);
+  let filteredData = newData
+  filteredData = filteredData.filter(names => names.Name === inputElement);
+  //console.log(filteredData)
+  var names = filteredData.map(name => name.Name);
+  if (inputElement in names === false){
+      alert("Sorry!! We can not find your name....");
+      return false;
+  }
+  // BONUS: Calculate summary statistics for the age field of the filtered data
+
+  // First, create an array with just the age values
+  var Occurrences = filteredData.map(names => names.Occurrence);
+  var genders = filteredData.map(gender => gender.Gender);
+  //console.log(genders);
+  
+
+  if (genders[0]==="F"){
+      alert("Yay!! This is a female name...");
+      var image = "/static/1457d79ae7aeb25.jpg"
+  }else {
+      alert("Oh, sounds more like a boy's name though...");
+      var image = "/static/5b6f65b04f21b3590172f8ce6dba33e6.jpg"
+  }
+
+  d3.select("#gender_image").attr("src", image);
+  // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
+  var mean = math.mean(Occurrences);
+  var median = math.median(Occurrences);
+  var mode = math.mode(Occurrences);
+  var variance = math.var(Occurrences);
+  var standardDeviation = math.std(Occurrences);
+
+  // Finally, add the summary stats to the `ul` tag
+  d3.select(".summary li").remove();
+  d3.select(".summary")
+    .append("li").text(`Mean: ${mean}`)
+    .append("li").text(`Median: ${median}`)
+    .append("li").text(`Mode: ${mode}`)
+    .append("li").text(`Variance: ${variance}`)
+    .append("li").text(`Standard Deviation: ${standardDeviation}`);
+
+  //console.log(filteredData);
+  });
+
+
 });
